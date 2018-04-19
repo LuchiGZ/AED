16614d3a766e7a77c916f9ffb1e225c5b041f578
LuchiGZ <gonzalez9luciano@gmail.com>
2018-04-19 09:00:04 -0300

    Add files via upload

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,17 @@
+/* Adicion
+ * Suma dos numeros ingresados por el usuario
+ * LG
+ * 20180419
+ */
+#include <iostream>
+int main() {
+	int a;
+	int b;
+	std::cout << "--Adicion de dos numeros--\nIngrese a:\n";
+	std::cin >> a;
+	std::cout << "Ingrese b:\n";
+	std::cin >> b;
+	std::cout << "a + b = ";
+	std::cout << a + b << "\n";
+	system("PAUSE");
+}
